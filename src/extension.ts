65295b9c058d9913cef3ed6c02b77e20a7b57756
alexlu07 <alexlu.manhasset@gmail.com>
2025-06-02 13:36:03 -0400

    extension basic functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate() {
+  vscode.window.onDidChangeActiveTextEditor(editor => {
+    if (editor && editor.document) {
+      const filePath = editor.document.uri.fsPath;
+      const envScriptPath = path.join('/tmp', 'update_editor_env.sh');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-editor-env" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-editor-env.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-editor-env!');
-	});
-
-	context.subscriptions.push(disposable);
+      try {
+        fs.writeFileSync(envScriptPath, `export EDITOR_PATH="${filePath.replace(/"/g, '\\"')}"\n`);
+      } catch (err) {
+        console.error("Failed to write EDITOR_PATH:", err);
+      }
+    }
+  });
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
